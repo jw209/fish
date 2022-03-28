036f6d9ca72da185cab5de35e0bd627c67de8e5e
Nathan M Hulet <nmh91@uakron.edu>
2022-03-28 19:37:54 -0400

    Add logistic regression model

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
 from statistical_summary import statistical_summary
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 matplotlib.use("TkAgg")
-
 
 fish_data = pd.read_csv('Fish.csv')
 
@@ -32,4 +35,27 @@ def show_graphs():
 
 statistical_summary(fish_data)
 
-show_graphs()
+# show_graphs()
+
+
+# split data into training and testing sets
+X = fish_data.drop('Species', axis=1)
+y = fish_data['Species']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# standardize the data
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# instantiate the model
+log_reg = LogisticRegression()
+# fit the model
+log_reg.fit(X_train, y_train)
+# predict the response
+y_pred = log_reg.predict(X_test)
+# confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+# accuracy score
+print(accuracy_score(y_test, y_pred))
+
+
