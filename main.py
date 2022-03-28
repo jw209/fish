da18eb0c9c1a928e5e85e4f51049efe79137c6bb
Jesse <77357219+jw209@users.noreply.github.com>
2022-03-28 18:55:18 -0400

    Add scatterplot of weight and height of fish

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-
 # code here
+import pandas as pd
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+fish_data = pd.read_csv('Fish.csv')
+
+plt.plot(fish_data["Weight"], fish_data["Height"], 'ro')
+plt.ylabel('Height')
+plt.xlabel('Weight')
+
+plt.show()
