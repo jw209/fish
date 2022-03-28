21e48f9ffa976eb9235b3e19e8ab6a18aac3c1d9
Nathan M Hulet <nmh91@uakron.edu>
2022-03-28 19:26:53 -0400

    Add function to show graphs

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 # code here
 import pandas as pd
+import seaborn as sns
 import matplotlib
-matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+
+matplotlib.use("TkAgg")
+
 
 fish_data = pd.read_csv('Fish.csv')
 
-plt.plot(fish_data["Weight"], fish_data["Height"], 'ro')
-plt.ylabel('Height')
-plt.xlabel('Weight')
 
-plt.show()
+# function to plot the data
+def show_graphs():
+    # pair plot
+    sns.pairplot(fish_data, hue='Species')
+    plt.figure(figsize=(12, 8))
+    plt.show()
+    # weight against height
+    plt.plot(fish_data["Weight"], fish_data["Height"], 'ro')
+    plt.ylabel('Height')
+    plt.xlabel('Weight')
+    plt.show()
+    # boxplot of Length1 by Species
+    sns.boxplot(x='Species', y='Length1', data=fish_data)
+    plt.show()
+    # pie-chart of number of species
+    plt.pie(fish_data['Species'].value_counts(), labels=fish_data['Species'].value_counts().index, autopct='%1.1f%%')
+    plt.show()
+
+
+show_graphs()
