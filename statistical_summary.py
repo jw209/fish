26076d23c5f4eee3499de1dd38c2db33eeb0ac42
Jesse <77357219+jw209@users.noreply.github.com>
2022-03-28 19:41:47 -0400

    Add median and mode for classes

--- statistical_summary.py
+++ statistical_summary.py
@@ -8,5 +8,11 @@ def statistical_summary(data):
         print(" {0} ".format(specie).center(80, '-'))
         divider()
         frame = data[data['Species'] == specie]
+        print(" {0} median: ".format(specie).center(80, '-'))
+        print(frame[['Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width']].median())
+        divider()
+        print(" {0} mode: ".format(specie).center(80, '-'))
+        print(frame[['Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width']].mode().head(1))
+        divider()
         print(frame.describe())
         divider()
